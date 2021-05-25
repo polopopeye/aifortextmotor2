8798a0e3cfd713e6117b559d9a166f3ea59f644f
Kenneth Suarez <33601506+polopopeye@users.noreply.github.com>
2021-05-25 22:41:41 +0200

    cambio a bing buscador
    cambio de buscador google para bing por formato en utf y extrae los headers de noticias correctamente,

--- src/modules/motor/extract.js
+++ src/modules/motor/extract.js
@@ -4,9 +4,12 @@ import jsdom from 'jsdom';
 
 const { JSDOM } = jsdom;
 
-let newsUrl = 'https://www.google.es/search';
+let query = 'Negocios';
+
+let newsUrl = 'https://www.bing.com/news/search';
+// let newsUrl = 'https://www.google.es/search';
+// https://duckduckgo.com/?q=ultimas+noticias&iar=news&ia=news
 // let newsUrl = 'https://www.google.es/search?btnG=Buscar&q=';
-let query = 'Covid España';
 // // let postUrl =
 //   '&biw=1536&bih=722&tbm=nws&sxsrf=ALeKk02qhT7sU3kSgBkU4jhLhFJJyRwUyw%3A1621873913040&ei=-dSrYMn4AdKYlwS_0LYw';
 // let urlApiRequest = newsUrl + encodeURIComponent(query) + postUrl;
@@ -20,8 +23,9 @@ const getData = async () => {
       headers: { 'Content-type': 'text/html; charset=utf-8' },
       params: {
         q: query,
-        source: 'lnms',
-        tbm: 'nws',
+        // source: 'lnms',
+        // tbm: 'nws',
+        FORM: 'HDRSC6',
       },
     })
     .then(function (response) {
@@ -34,26 +38,14 @@ const getData = async () => {
 
   console.log('RESPONSED');
   //   let data = $(responseBody);
-
+  let tagname = 'title';
   const dom = new JSDOM(responseBody);
-  headersFromVirtualDom = dom.window.document.getElementsByTagName('h3');
+  //   headersFromVirtualDom = dom.window.document.getElementsByTagName(tagname);
+  headersFromVirtualDom = dom.window.document.getElementsByClassName(tagname);
+
   for (let e of headersFromVirtualDom) {
     console.log(e.textContent);
   }
-
-  //   console.log(dom.window.document.getElementsByTagName('h3').length); // "Hello world"
-
-  //   console.log(dom.jQuery('p').textContent); // "Hello world"
-
-  //   let responseBody2 = new jsdom(responseBody);
-
-  //   console.log($('h2'));
-
-  //   headersFromRegex = responseBody.match(getHeaders);
-
-  //   console.log(headersFromRegex[1]);
 };
 
 getData();
-
-// console.log(axios);
